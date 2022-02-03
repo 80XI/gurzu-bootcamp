890c8320247e37e86e479960f2f4e683a6e8996c
casper <salilkayastha1490@gmail.com>
2022-02-03 17:52:10 +0545

    calculator is functional, but validation is missing

--- Calculator/calculator.rb
+++ Calculator/calculator.rb
@@ -1,53 +1,62 @@
 operation = ["1. Addition", "2. Subtraction", "3. Division", "4. Multiplication"]
 puts operation[0..3]
-in_operation = gets.chomp
+in_operation = gets.chomp.to_i
 ary =  in_operation.to_i-1
 puts operation[ary]
+fun = in_operation.to_i
+
 
 puts "Enter first number"
-num1 = gets.chomp
+$num1 = gets.chomp.to_i
 puts "Enter second number"
-num2 = gets.chomp
+$num2 = gets.chomp.to_i
 
-if in_operation == 1 then
-    addition_function
-end
-if in_operation == 2 then
-    subtraction_function
-end
-if in_operation == 3 then
-    division_function
-end
-if in_operation == 4 then
-    multiplication_function
-end
-
-
-def addition_function
-    @n1 = num1
-    @n2 = num2
+def addition_function(n1, n2)
+    @n1 = n1
+    @n2 = n2
     @result = @n1 + @n2
     puts "The answer is #{@result}"
 end
 
-def subtraction_function
-    @n1 = num1
-    @n2 = num2
+def subtraction_function(n1, n2)
+    @n1 = $num1
+    @n2 = $num2
     @result = @n1 - @n2
     puts "The answer is #{@result}"
 end
 
-def division_function
-    @n1 = num1
-    @n2 = num2
+def division_function(n1, n2)
+    @n1 = $num1
+    @n2 = $num2
     @result = @n1 / @n2
     puts "The answer is #{@result}"
 end
 
-def multiplication_function
-    @n1 = num1
-    @n2 = num2
+def multiplication_function(n1, n2)
+    @n1 = $num1
+    @n2 = $num2
     @result = @n1 * @n2
     puts "The answer is #{@result}"
 end
-#clear screen?
+
+if fun == 1 then
+    addition_function($num1, $num2)
+    
+end
+if fun == 2 then
+    result = $num1 - $num2
+    puts "answer #{result}"
+end
+if fun == 3 then
+    result = $num1 / $num2
+    puts "answer #{result}"
+end
+if fun == 4 then
+    result = $num1 * $num2
+    puts "answer #{result}"
+end
+
+
+
+
+# #clear screen?
